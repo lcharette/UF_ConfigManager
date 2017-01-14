e77de897c5bacf81d0f46f9d77a838914bc9938d
Louis Charette <charette.louis@gmail.com>
2017-01-14 13:06:08 -0500

    Support for AdminLTE the default theme

---
+++ locale/en_US/AdminLTE.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "ADMINLTE" => [
+        "SETTINGS" => [
+            "@TRANSLATION" => "AdminLTE Settings",
+
+            "DESC" => "Settings for the AdminLTE Theme",
+
+            "SKIN" => "Skin color"
+        ],
+
+        "SKIN" => [
+            "BLUE" => "Blue",
+            "BLACK" => "Black",
+            "PURPLE" => "Purple",
+            "GREEN" => "Green",
+            "RED" => "Red",
+            "YELLOW" => "Yellow",
+            "BLUE_LIGHT" => "Blue Light",
+            "BLACK_LIGHT" => "Black Light",
+            "PURPLE_LIGHT" => "Purple Light",
+            "GREEN_LIGHT" => "Green Light",
+            "RED_LIGHT" => "Red Light",
+            "YELLOW_LIGHT" => "Yellow Light"
+        ]
+    ]
+];
---
+++ locale/fr_FR/AdminLTE.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "ADMINLTE" => [
+        "SETTINGS" => [
+            "@TRANSLATION" => "Paramètres d'AdminLTE",
+
+            "DESC" => "Paramètres du thème AdminLTE",
+
+            "SKIN" => "Couleur du thème"
+        ],
+
+        "SKIN" => [
+            "BLUE" => "Bleu",
+            "BLACK" => "Noir",
+            "PURPLE" => "Violet",
+            "GREEN" => "Vert",
+            "RED" => "Rouge",
+            "YELLOW" => "Jaune",
+            "BLUE_LIGHT" => "Bleu (Light)",
+            "BLACK_LIGHT" => "Noir (Light)",
+            "PURPLE_LIGHT" => "Violet (Light)",
+            "GREEN_LIGHT" => "Vert (Light)",
+            "RED_LIGHT" => "Rouge (Light)",
+            "YELLOW_LIGHT" => "Jaune (Light)"
+        ]
+    ]
+];
